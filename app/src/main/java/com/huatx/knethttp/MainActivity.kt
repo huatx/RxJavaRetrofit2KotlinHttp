fbb8babcae118640bf4d0786d6a815533707313d
葱花 <xiaoyao_2009@126.com>
2018-08-24 17:14:04 +0800

    实际的例子

--- app/src/main/java/com/huatx/knethttp/MainActivity.kt
+++ app/src/main/java/com/huatx/knethttp/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.huatx.knethttp
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.huatx.netwrok.HttpSimpleSubscriber
+import com.huatx.netwrok.ProgressSubscriber
+import com.trello.rxlifecycle2.components.support.RxAppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : RxAppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        LoginModel.getVeraCode("", "", this@MainActivity, object : HttpSimpleSubscriber<String>() {
+            override fun _onNext(retData: String?) {
+
+            }
+
+            override fun _onError(errorCode: Int, msg: String) {
+            }
+        })
+        LoginModel.getVeraCode("", "", this, object : ProgressSubscriber<String>(this@MainActivity, "xxxx"){
+            override fun _onNext(retData: String?) {
+
+            }
+
+            override fun _onError(errorCode: Int, msg: String) {
+            }
+        })
+
+
     }
 }
--- netwrok/src/main/java/com/huatx/netwrok/consts/UriConsts.kt
+++ netwrok/src/main/java/com/huatx/netwrok/consts/UriConsts.kt
@@ -2,5 +2,6 @@ package com.huatx.netwrok.consts
 
 object UriConsts {
     //    val BASE_URL = ""
+    //要获取的网络URL
     const val BASE_URL = ""
 }
